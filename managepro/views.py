8d8bb7295bf0c768a6d448f6313cfc83f8531eef
Kirill <karachevkir@yandex.ru>
2025-06-04 23:07:20 +0300

    syle catigories

--- managepro/views.py
+++ managepro/views.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.shortcuts import render
 from unicodedata import category
 
+from django.db.models import Count
 from .models import *
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
@@ -17,5 +18,16 @@ def views_products(request):
 
 
 def views_categories(request):
-    category = Category.objects
-    return render(request, 'categories.html', {'category': category})
+    query = request.GET.get('search', '')
+
+    # Поиск категорий по имени
+    category = Category.objects.filter(categoryname__icontains=query).order_by('categoryname').distinct()
+
+    # Агрегатная функция — сколько продуктов в каждой категории
+    category_with_counts = Category.objects.annotate(product_count=Count('product')).order_by('categoryname')
+
+    return render(request, 'categories.html', {
+        'category': category,
+        'category_with_counts': category_with_counts,
+        'query': query,
+    })
